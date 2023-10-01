c2fdd144e5b22c74e90f5a1019d2aa0db5e1e190
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-10-01 20:30:01 +1300

    footer done

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          One of three columns <br>
+          some text <br>
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          Maybe some icons
+        </div>
+      </div>
+    </div>
+  </footer>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 include_once("includes/header.php");
 include_once("includes/nav.php");
+include_once("includes/footer.php");
 ?> 
 
 <!DOCTYPE html>
