a0b8be69b046859b3ad923a276c3cc3c088a6d9a
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-10-01 20:05:48 +1300

    default navbar sorted

---
+++ includes/nav.php
@@ -0,0 +1,62 @@
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+    aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">
+    <img src="images/logo.png" width="250" height="65" class="d-inline-block align-top" alt="">
+  </a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a href="#"></a><button class="btn btn-success my-2 my-sm-0" id="NavButton">Infiltrate Help</button>
+      </li>
+      <li class="nav-item">
+        <a href="#"></a><button class="btn btn-info my-2 my-sm-0" id="NavButton">Shop</button>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-info my-2 my-sm-0" id="NavButton">Categories</button>
+        <!--Add dropdown here-->
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-info my-2 my-sm-0" id="NavButton">Contact us</button>
+      </li>
+    </ul>
+
+    <div class="cart-icon">
+      <a href="#" class="navbar-brand">
+        <img src="images/Vector.png" width="32" height="32" alt="Cart">
+        <span class="badge">0</span>
+        <span class="displaytext">$0.00</span>
+      </a>
+    </div>
+
+    <a class="flex-sm-fill text-sm-right" href="#"><button class="btn btn-info my-2 my-sm-0" id="NavButton">Login</button></a>
+    <a class="flex-sm-fill text-sm-right" href="#"><button class="btn btn-success my-2 my-sm-0" id="NavButton">Register</button></a>
+  </div>
+</nav>
+
+<div class="container mt-2">
+  <div class="row">
+    <div class="col-lg-2">
+
+      <button class="btn btn-info my-2 my-sm-0" id="NavButton">Filter</button>
+    </div>
+    <div class="col-lg-6 offset-lg-4">
+      <form class="form-inline my-2 my-lg-0 justify-content-end">
+        <div class="input-group">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-info my-2 my-sm-0" type="submit">Search</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include_once("includes/header.php");
+include_once("includes/nav.php");
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+</body>
+</html>
