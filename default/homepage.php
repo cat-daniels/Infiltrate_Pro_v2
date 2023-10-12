3242611c7ad02c62294a9f67d46c2824d5a28c0e
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-10-12 20:03:22 +1300

    added product cards altering display

--- default/homepage.php
+++ default/homepage.php
@@ -1,13 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php 
 include_once("../includes/header.php");
 include_once("../includes/nav.php");
 include_once("../includes/footer.php");
-
-?> 
-
-<!DOCTYPE html>
-<html lang="en">
+?>
 <body>
-<h1>Is this working?</h1>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "infiltratepro"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    // going to put this into columns
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="card" style="width: 18rem;">';
+        // Display the image if available
+        if (!empty($row['Image'])) {
+            $imagePath = '../images/' . $row['Image']; // Construct the full path
+            echo '<img src="' . $imagePath . '" class="card-img-top" alt="' . $row['ProductName'] . '">';
+        } else {
+            echo '<img src="placeholder_image.jpg" class="card-img-top" alt="No Image">';
+        }
+        echo '<div class="card-body">';
+        echo '<h5 class="card-title">' . $row['ProductName'] . '</h5>';
+        echo '<p class="card-text">Price: $' . $row['Price'] . '</p>';
+        echo '<p class="card-text">Description: ' . $row['Description'] . '</p>';
+        echo '</div>';
+        echo '</div>';
+    }
+} else {
+    echo "No products found.";
+}
+
+$conn->close();
+?>
 </body>
 </html>
