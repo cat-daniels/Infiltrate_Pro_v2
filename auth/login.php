de4e563013dda344813cc33122525b460f1d65bc
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-10-15 22:29:39 +1300

    just adding in the login form

--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once("../includes/header.php");
 include_once("../includes/nav.php");
@@ -5,6 +6,26 @@ include_once("../includes/footer.php");
 
 
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    
-<link rel="stylesheet" href="../style/style.css">
-<h1>test login success</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+  <div class="loginform">
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<a href="register.php"><button class="btn btn-outline-success" style = "margin: 30px;">Register account instead</button></a>
+</div>
+</body>
+</html>
