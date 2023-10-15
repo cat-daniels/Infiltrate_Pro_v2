c8958d5bd7db4172fb707ca46f9c0b8661fded3c
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-10-15 21:39:06 +1300

    I'm not sure what happened but fixed

--- default/homepage.php
+++ default/homepage.php
@@ -4,14 +4,12 @@
 include_once("../includes/header.php");
 include_once("../includes/nav.php");
 
-
-// functions included:
+//Functions
 include_once("../functions/productfunctions.php");
-?>
+?> 
+
 <body>
-<?php
-displayproductincard();
-?>
+
 </body>
 
 <?php include_once("../includes/footer.php"); ?>
