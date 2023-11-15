a6e1a7f9a24f953def31412c7d0b87beb6a3cff7
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-11-15 15:01:36 +1300

    Revert "login & registration incorporated session tokens"
    This reverts commit 236a5755ceb92139c801fbd28fbf28924b028eea.

--- Pages/login.php
+++ Pages/login.php
@@ -4,42 +4,10 @@ include_once("../Public/header.php");
 include_once("../Components/nav.php");
 
 //Functions
-
-include_once("../Functions/authfunctions.php");
-
-
-// Check if the login form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"]; // Capture user input for email
-    $password = $_POST["password"]; // Capture user input for password
-
-    // Call the LoginAccount function with user input
-    $loginResult = LoginAccount($email, $password);
-
-    // Handle potential error messages
-    if ($loginResult !== null) {
-        // Display error message if login failed
-        echo '<p style="color: red;">' . $loginResult . '</p>';
-    }
-}
-?>
+?> 
 
 <body>
-    <div class="loginform">
-        <form method="post" action="login.php">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-        <a href="register.php"><button class="btn btn-outline-success" style="margin: 30px;">Register account instead</button></a>
-    </div>
+<h1>Login</h1>
 </body>
 
 <?php include_once("../Components/footer.php"); ?>
