b49031e123e198853761a37cbc77dfeb6a3ead49
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2023-11-23 00:28:56 +1300

    xss implemented

--- Components/items.php
+++ Components/items.php
@@ -20,16 +20,35 @@ function displaySearchBar() {
     echo '<div class="bg-info text-light p-2 rounded mb-3">';
     echo '<h5>Search up items</h5>';
     echo '</div>';
+    echo '<form action="" method="POST">'; // Opening form tag
     echo '<div class="input-group rounded-pill shadow-sm" style="max-width: 400px;">';
-    echo '<input class="form-control rounded-pill border-1" type="search" placeholder="Search" aria-label="Search" name="query">';
+    echo '<input class="form-control rounded-pill border-0" type="search" placeholder="Search" aria-label="Search" name="query">';
     echo '<div class="input-group-append">';
-    echo '<button class="btn btn-outline-success rounded-pill" type="submit">Search</button>';
+    echo '<button class="btn btn-outline-secondary rounded-pill" type="submit">Search</button>';
     echo '</div>';
     echo '</div>';
+    echo '</form>'; // Closing form tag
     echo '</div>';
 }
 
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if the 'query' parameter is present in the POST data
+    if (isset($_POST['query'])) {
+        // Retrieve and sanitize the user input
+        $userInput = $_POST['query'];
+        
+        echo '<div class="d-flex flex-column align-items-center mt-4"> <h5>';
+        echo $userInput;
+        echo '</h5></div>';
+        // Display the user input
+        echo '<script>';
+        echo 'setTimeout(function() {';
+        echo 'window.location.href = window.location.pathname;';
+        echo '}, 2000);';
+        echo '</script>';
+       
+    }
+}
 
 //These functions are so I don't have to keep writing that I would like to have a gap in place lol
 function topgap(){
